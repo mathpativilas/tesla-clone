00719d07f45471c5a4dab7d6faafcbae34d1a2b1
vilas mathpati <atechy.vilas@gmail.com>
2022-05-21 20:30:17 +0400

    home page completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,41 @@ import React from "react";
 import styled from "styled-components";
 import Section from "./Section";
 
+
 function Home() {
+
+
+
+
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        leftbtntext="custom order"
+        rightbtntext="Existing Inventory"
+        backimg="model-s.jpg"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        leftbtntext="custom order"
+        rightbtntext="Existing Inventory"
+        backimg="model-3.jpg"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        leftbtntext="custom order"
+        rightbtntext="Existing Inventory"
+        backimg="model-x.jpg"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        leftbtntext="custom order"
+        backimg="model-y.jpg"
+      />
     </Container>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({ title, description, leftbtntext, rightbtntext, backimg }) {
   return (
-    <Wrap>
+    <Wrap backimg={backimg}>
       <Itemtext>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery </p>
+        <h1>{title}</h1>
+        <p>{description} </p>
       </Itemtext>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftbtntext}</LeftButton>
 
-      <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
+          {rightbtntext && <RightButton>{rightbtntext}</RightButton>}
+        </ButtonGroup>
+
+        <DownArrow src="./images/down-arrow.svg" />
+      </Buttons>
     </Wrap>
   );
 }
@@ -22,23 +26,58 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  text-align: center;
-  background-image: url("./images/model-s.jpg");
+
+  background-image: ${(props) => `url("./images/${props.backimg}")`};
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Itemtext = styled.div`
   padding-top: 15vh;
+  text-align: center;
 `;
 
-const ButtonGroup = styled.div``;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 const LeftButton = styled.div`
   width: 256px;
   height: 40px;
-  background-color: black;
+  background-color: rgba(23, 26, 32, 0.8);
+  border-radius: 100px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+
+  margin: 8px;
 `;
 
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  color: black;
+  opacity: 0.65;
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+
+const Buttons = styled.div``;
